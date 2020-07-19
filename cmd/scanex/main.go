5c06ff003e8d7dfa7c8bed0cdbafd5c8a94dd3d2
Joshua T Corbin <jcorbin@wunjo.org>
2020-07-19 10:42:35 -0700

    scanex: globalize main state

--- cmd/scanex/main.go
+++ cmd/scanex/main.go
@@ -15,49 +15,39 @@ import (
 	"github.com/jcorbin/soc/scandown"
 )
 
-func main() {
-	var (
-		in      = os.Stdin
-		out     = &socutil.ErrWriter{Writer: os.Stdout}
-		hexdump bool
-		blanks  bool
-		raw     bool
-		verbose bool
-	)
+var (
+	in      = os.Stdin
+	out     = &socutil.ErrWriter{Writer: os.Stdout}
+	hexdump bool
+	blanks  bool
+	raw     bool
+	verbose bool
 
-	flag.BoolVar(&blanks, "blanks", false, "print blank tokens too")
-	flag.BoolVar(&hexdump, "hex", false, "hexdump content rather than quote")
-	flag.BoolVar(&raw, "raw", false, "dump raw block content, don't strip marks")
-	flag.BoolVar(&verbose, "v", false, "enable verbose output")
+	logOut  *socutil.Prefixer
+	scanner *bufio.Scanner
+	blocks  scandown.BlockStack
+)
+
+func main() {
 	flag.Parse()
 
-	logOut := socutil.PrefixWriter("> log: ", out)
+	logOut = socutil.PrefixWriter("> log: ", out)
 	defer logOut.Close()
 	log.SetOutput(logOut)
 	log.SetFlags(0)
 
-	addLogPrefix := func(prefix string) func() {
-		old := logOut.Prefix
-		logOut.Prefix = old + prefix
-		return func() {
-			logOut.Prefix = old
-		}
-	}
+	scanner = bufio.NewScanner(in)
+	scanner.Split(blocks.Scan)
 
-	var blocks scandown.BlockStack
-
-	sc := bufio.NewScanner(in)
-	sc.Split(blocks.Scan)
-
-	n := 0
+	tokenCount := 0
 	if err := socutil.WriteLines(out, func(w io.Writer, _ func()) bool {
-		if !sc.Scan() {
+		if !scanner.Scan() {
 			return false
 		}
 
 		var body io.ReadSeeker = &blocks
 		if raw {
-			body = bytes.NewReader(sc.Bytes())
+			body = bytes.NewReader(scanner.Bytes())
 		} else if n, err := body.Seek(0, io.SeekEnd); err != nil {
 			log.Fatalf("body seek error: %v", err)
 		} else if n == 0 {
@@ -67,8 +57,8 @@ func main() {
 		}
 
 		if body != nil || blanks {
-			n++
-			width, _ := fmt.Fprintf(w, "%v. ", n)
+			tokenCount++
+			width, _ := fmt.Fprintf(w, "%v. ", tokenCount)
 			itemOut := socutil.PrefixWriter(strings.Repeat(" ", width), w)
 			itemOut.Skip = true
 			defer itemOut.Close()
@@ -102,9 +92,23 @@ func main() {
 	}); err != nil {
 		log.Fatalf("write error: %v", err)
 	}
-
-	if err := sc.Err(); err != nil {
+	if err := scanner.Err(); err != nil {
 		fmt.Printf("# main scan error\n%T: %v\n", err, err)
 		os.Exit(1)
+	}
+}
+
+func init() {
+	flag.BoolVar(&blanks, "blanks", false, "print blank tokens too")
+	flag.BoolVar(&hexdump, "hex", false, "hexdump content rather than quote")
+	flag.BoolVar(&raw, "raw", false, "dump raw block content, don't strip marks")
+	flag.BoolVar(&verbose, "v", false, "enable verbose output")
+}
+
+func addLogPrefix(prefix string) func() {
+	old := logOut.Prefix
+	logOut.Prefix = old + prefix
+	return func() {
+		logOut.Prefix = old
 	}
 }
