c18b0df6e777fbdbb0c8d32bf1e76c197569d402
Joshua T Corbin <jcorbin@wunjo.org>
2020-07-19 10:53:06 -0700

    scanex: refactor body flow

--- cmd/scanex/main.go
+++ cmd/scanex/main.go
@@ -34,49 +34,57 @@ func scan() {
 
 	tokenCount := 0
 	for out.Err == nil && scanner.Scan() {
+		// skip any blank tokens (block open/close notifications) unless flagged to not
+		if !blanks {
+			n, err := blocks.Seek(0, io.SeekEnd)
+			if err == nil {
+				_, err = blocks.Seek(0, io.SeekStart)
+			}
+			if err != nil {
+				log.Fatalf("BlockStack.Seek failed: %v", err)
+			}
+			if n == 0 {
+				continue
+			}
+		}
 
+		// write an ordered bullet, compute hanging indent, and setup so
+		// that subsequent output and logs are demarcated under it
+		tokenCount++
+		width, _ := fmt.Fprintf(&outBuf, "%v. ", tokenCount)
+		itemOut := socutil.PrefixWriter(strings.Repeat(" ", width), &outBuf)
+		itemOut.Skip = true
+		defer itemOut.Close()
+		defer addLogPrefix(itemOut.Prefix)()
+
+		// write the BlockStack state, as a sub-ordered-list when running verbose
+		if verbose {
+			fmt.Fprintf(itemOut, "%+v\n", blocks)
+		} else {
+			fmt.Fprintf(itemOut, "%v\n", blocks)
+		}
+
+		// write the block token body through into a codefenced hexdump, or blockquoted otherwise
 		var body io.ReadSeeker = &blocks
 		if raw {
 			body = bytes.NewReader(scanner.Bytes())
-		} else if n, err := body.Seek(0, io.SeekEnd); err != nil {
-			log.Fatalf("body seek error: %v", err)
-		} else if n == 0 {
-			body = nil
+		}
+		if hexdump {
+			io.WriteString(itemOut, "```hexdump\n")
+			dumper := hex.Dumper(itemOut)
+			io.Copy(dumper, body)
+			dumper.Close()
+			io.WriteString(itemOut, "```\n")
 		} else {
-			body.Seek(0, io.SeekStart)
+			pw := socutil.PrefixWriter("> ", itemOut)
+			io.Copy(pw, body)
+			pw.Close()
+		}
+		if b := itemOut.Buffer.Bytes(); len(b) > 0 && b[len(b)-1] != '\n' {
+			io.WriteString(itemOut, "\n")
 		}
 
-		if body != nil || blanks {
-			tokenCount++
-			width, _ := fmt.Fprintf(&outBuf, "%v. ", tokenCount)
-			itemOut := socutil.PrefixWriter(strings.Repeat(" ", width), &outBuf)
-			itemOut.Skip = true
-			defer itemOut.Close()
-			defer addLogPrefix(itemOut.Prefix)()
-
-			if verbose {
-				fmt.Fprintf(itemOut, "@%v block stack:\n%+v\n", blocks.Offset(), blocks)
-			} else {
-				fmt.Fprintf(itemOut, "%v\n", blocks)
-			}
-
-			if body != nil {
-				if hexdump {
-					io.WriteString(itemOut, "```hexdump\n")
-					dumper := hex.Dumper(itemOut)
-					io.Copy(dumper, body)
-					dumper.Close()
-					io.WriteString(itemOut, "```\n")
-				} else {
-					pw := socutil.PrefixWriter("> ", itemOut)
-					io.Copy(pw, body)
-					pw.Close()
-				}
-				if b := itemOut.Buffer.Bytes(); len(b) > 0 && b[len(b)-1] != '\n' {
-					io.WriteString(itemOut, "\n")
-				}
-			}
-		}
+		// flush output, aligned to line boundaries
 		outBuf.MaybeFlush()
 	}
 }
